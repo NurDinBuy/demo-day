8ed854e1f84c6d31f9e3cc3eb4e3e94821385961
NurDinBuy <94064993+NurDinBuy@users.noreply.github.com>
2025-06-28 15:50:04 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ const bindPostData = (form) => {
             return alert('Вы не повторили пароль!')
         }
         if (password.value === passwordRepeat.value) {
-            return postData('https://2a61-212-112-103-156.ngrok-free.app/auth/register/', json)
+            return postData('https://7d00-31-186-49-201.ngrok-free.app/auth/register/', json)
         }
         else {
             return alert('Пароли не совпали!')
